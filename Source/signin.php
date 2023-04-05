9a07c760a97fa62cafc6be562fbaa6f5e6740e82
Silicon Dioxide <89680161+silicondioxide-sio2@users.noreply.github.com>
2023-04-05 13:18:09 +0800

    complete the sign in file

--- Source/signin.php
+++ Source/signin.php
@@ -2,6 +2,14 @@
 <form method="POST" action="signin_semak.php">
 <div>
     Username = : <br>
-    <input type ="text" name = "username" placeholder="Sila isi Username" 
-    maxlength = "255" size = "15%" required autofocus /> 
+    <input type ="text" name = "username" placeholder="Sila isi Username" maxlength = "255" size = "15%" required autofocus /> 
     <br>
+    Password = : <br>
+    <input type ="password" name="password" placeholder="*************" size="15%" required autofocus />
+</div>
+<div>
+    <button name = "Submit" type="submit">LOGIN</button>
+    <button type="reset">RESET</button>
+
+</div>
+</form>
